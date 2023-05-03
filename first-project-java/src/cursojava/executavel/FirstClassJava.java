f11a31b03fa3cd230b488edbd3d6e3f334013bcd
italo <italo@FnsHome>
2023-05-03 18:32:12 -0300

    test commit

---
+++ first-project-java/src/cursojava/executavel/FirstClassJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class FirstClassJava {
+	
+	/*Main é um metodo auto executavel em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java");
+	}
+}
